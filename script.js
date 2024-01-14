d54215e8eed01ebba65b68564e8f5ec4835c5235
beekashn <git7beekash7@gmail.com>
2024-01-14 22:05:12 +0545

    commit

--- script.js
+++ script.js
@@ -8,13 +8,9 @@ const player2 = document.getElementById("player--1");
 const resetBtn = document.querySelector(".reset-btn");
 const result = document.querySelector("#msg");
 
-let state = 0; //first plater turn
+let state = 0; //first player turn
 gameSpace.forEach(function (space) {
-  // console.log(space);
   space.addEventListener("click", function (event1) {
-    // console.log(event1);
-    // console.log("ji");
-    // player1.innerText = "";
     if (state === 0) {
       result.innerText = "Playing...";
       player1.innerText = "";
@@ -31,22 +27,6 @@ gameSpace.forEach(function (space) {
       space.style.color = "white";
       space.style.fontSize = "1.5rem";
       state = 1; // pass the turn to 2nd player
-
-      // result.innerText = "Playing...";
-      // player.innerText = "";
-      // player.innerText = "player 2 turn";
-      // player.style.backgroundColor = "green";
-      // player.style.backgroundColor = "transparent";
-      // player.style.color = "white";
-      // space.style.boxShadow = "inset -2px 2px 10px black";
-      // space.style.borderRadius = "20px";
-      // space.style.transition = "all ease 0.2s";
-      // space.style.backgroundColor = "blue";
-      // space.disabled = true;
-      // space.innerText = "O";
-      // space.style.color = "white";
-      // space.style.fontSize = "1.5rem";
-      // state = 1; // pass the turn to 2nd player
     } else {
       player2.innerText = "";
       player1.innerText = "player 1 turn";
@@ -61,24 +41,7 @@ gameSpace.forEach(function (space) {
       space.innerText = "X";
       space.style.color = "white";
       space.style.fontSize = "1.5rem";
-
-      state = 0; // past the turn to 1st player again
-
-      // player.innerText = "";
-      // player.innerText = "player 1 turn";
-      // player.style.backgroundColor = "blue";
-      // player.style.backgroundColor = "transparent";
-      // player.style.color = "white";
-      // space.style.boxShadow = "inset -2px 2px 10px black";
-      // space.style.borderRadius = "20px";
-      // space.style.transition = "all ease 0.2s";
-      // space.style.backgroundColor = "green";
-      // space.disabled = true;
-      // space.innerText = "X";
-      // space.style.color = "white";
-      // space.style.fontSize = "1.5rem";
-
-      // state = 0;
+      state = 0; // pass the turn to 1st player again
     }
 
     checkWinner();
@@ -102,8 +65,6 @@ const gameReset = function () {
     player1.style.color = "black";
     player2.style.color = "black";
   });
-
-  // result.innerHTML='Congratulation you won'
 };
 resetBtn.addEventListener("click", gameReset);
 
@@ -123,31 +84,20 @@ const checkWinner = function () {
     const position1 = gameSpace[pattern[0]].innerText;
     const position2 = gameSpace[pattern[1]].innerText;
     const position3 = gameSpace[pattern[2]].innerText;
-    // console.log(position1);
     if (position1 !== "" && position2 !== "" && position3 !== "") {
       if (position1 === position2 && position2 === position3) {
-        // console.log("winner");
-        // gameSpace.disabled = true;
         result.innerText = `Congratulation !🥇`;
         player1.innerText = `player ${position1} won`;
         player2.innerText = `player ${position1} won`;
-
         gameSpace.forEach(function (space) {
-          // console.log(space.innerText);
-
           space.disabled = true;
           resetBtn.innerText = "New Game";
-
           resetBtn.addEventListener("click", function () {
             resetBtn.innerText = "Reset Game";
             result.innerText = `Let's Play`;
-            // player1.innerText = `Player 1 : O`;
-            // player2.innerText = `Player 1 : X`;
             gameReset();
           });
         });
-        // resetBtn.innerText = "Reset Game";
-        // gameSpace.disabled = true;
       }
     }
   }
